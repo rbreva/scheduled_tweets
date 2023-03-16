5645a2f7e9af96193896c85ab4197f12c380aca6
Mauricio Mendoza <rbreva@gmail.com>
2023-03-15 23:56:56 -0500

    Creating a sign form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 
 end
