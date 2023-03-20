5a7103d3c28f322109998a411c36f0487720f655
Mauricio Mendoza <rbreva@gmail.com>
2023-03-19 22:16:55 -0500

    Login with sessions cookies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         redirect_to root_path, notice: 'User was successfully created'
       else
         flash[:alert] = "Something was wrong with your registration"
